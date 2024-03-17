6650ababa90d29c5d33f1cb1db1b5a823daaaaaa
hagenimanayaphet <153589687+hagenimanayaphet@users.noreply.github.com>
2024-03-17 09:07:34 -0700

    Update index.php
    addedd codes to manage time

--- index.php
+++ index.php
@@ -5,6 +5,12 @@
 <?php
 echo "claude oyeeee!";
 ?>
+<?php
+echo "Today is " . date("Y/m/d") . "<br>";
+echo "Today is " . date("Y.m.d") . "<br>";
+echo "Today is " . date("Y-m-d") . "<br>";
+echo "Today is " . date("l");
+?>
 
 </body>
 </html>
